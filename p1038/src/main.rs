5ae2df15d819a1605980b927880a2ddcb4414645
brightchul <abhbtbb1@gmail.com>
2021-11-25 01:33:30 +0900

    feat: change code
    The previous approach was not appropriate, so I took another approach by referring to other people's solutions.
    
    reference : https://yabmoons.tistory.com/553

--- p1038/src/main.rs
+++ p1038/src/main.rs
@@ -1,35 +1,39 @@
-use std::io;
+use std::{collections::VecDeque, io};
 
 fn main() {
   let mut s = String::new();
   io::stdin().read_line(&mut s).unwrap();
-  let n: i32 = s.trim().parse().unwrap();
+  
+  let  n: i32 = s.trim().parse().unwrap();
 
-  if n > 1022 {
-    println!("-1");
+  if n < 10 {
+    println!("{}", n);
     return;
   }
 
-
-  let mut list = Vec::<Vec<i32>>::new();
-
-  // 10 ~ 9x
-  let ten0: Vec<i32> = (0..10).map(|v| if v == 0 {0} else {1}).collect();
-  list.push(ten0);
-
-  let ten1: Vec<i32> = (0..10).map(|v| v).collect();
-  list.push(ten1);
-  for one in 2..13 {
-
-    let one = one as usize;
-    let mut temp: Vec<i32> = vec![0];
-    
-    for idx in 1..10 {
-      let idx = idx as usize;
-      let value = temp[idx-1] + list[one-1][idx-1]; 
-      temp.push(value);
-    }
-    list.push(temp);
+  let mut queue: VecDeque<i64> = VecDeque::new();
+  for idx in 1..10 {
+    queue.push_back(idx);
   }
+
+  let mut count = 9;
   
+  while queue.len() > 0 {
+    let queue_front_value = queue.pop_front().unwrap();
+    let digit_limit = queue_front_value % 10;
+
+    for one in 0..digit_limit {
+      let generated_value = queue_front_value * 10 + one;
+      queue.push_back(generated_value);
+      count +=1;
+
+      if n == count {
+        println!("{}", generated_value);
+        return;
+      }
+    }
+  }
+
+  println!("-1");
+  return;
 }
