bc30bb5e543093a68ab9173675da1d7fecaf9491
brightchul <abhbtbb1@gmail.com>
2021-11-25 01:49:51 +0900

    feat: separate module for testable code

--- p1038/src/lib.rs
+++ p1038/src/lib.rs
@@ -1,8 +1,30 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use std::collections::VecDeque;
+
+pub fn solution(n: i32) -> i64 {
+    if n < 10 {
+        return n as i64;
     }
+
+    let mut queue: VecDeque<i64> = VecDeque::new();
+    for idx in 1..10 {
+        queue.push_back(idx);
+    }
+
+    let mut count = 9;
+
+    while queue.len() > 0 {
+        let queue_front_value = queue.pop_front().unwrap();
+        let digit_limit = queue_front_value % 10;
+
+        for one in 0..digit_limit {
+            let generated_value = queue_front_value * 10 + one;
+            queue.push_back(generated_value);
+            count += 1;
+
+            if n == count {
+                return generated_value;
+            }
+        }
+    }
+    -1
 }
--- p1038/src/main.rs
+++ p1038/src/main.rs
@@ -1,39 +1,11 @@
-use std::{collections::VecDeque, io};
+use std::io;
+
+use p1038::solution;
 
 fn main() {
   let mut s = String::new();
   io::stdin().read_line(&mut s).unwrap();
-  
+
   let  n: i32 = s.trim().parse().unwrap();
-
-  if n < 10 {
-    println!("{}", n);
-    return;
-  }
-
-  let mut queue: VecDeque<i64> = VecDeque::new();
-  for idx in 1..10 {
-    queue.push_back(idx);
-  }
-
-  let mut count = 9;
-  
-  while queue.len() > 0 {
-    let queue_front_value = queue.pop_front().unwrap();
-    let digit_limit = queue_front_value % 10;
-
-    for one in 0..digit_limit {
-      let generated_value = queue_front_value * 10 + one;
-      queue.push_back(generated_value);
-      count +=1;
-
-      if n == count {
-        println!("{}", generated_value);
-        return;
-      }
-    }
-  }
-
-  println!("-1");
-  return;
+  println!("{}", solution(n));
 }
