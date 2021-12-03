0ba9f855be4117532a61ff722f433132e4567191
brightchul <57323359+brightchul@users.noreply.github.com>
2021-12-04 01:02:37 +0900

    add: p1068 tree code using BTreeMap

--- p1068/src/lib.rs
+++ p1068/src/lib.rs
@@ -1,8 +1,56 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use std::{cmp, collections::BTreeMap, io};
+
+pub fn get_number_io() -> i32 {
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).unwrap();
+    let result = buf.trim().parse().unwrap();
+    result
+}
+
+pub fn get_vec_i32_io() -> Vec<i32> {
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).unwrap();
+    buf.split_whitespace()
+        .map(|v| v.parse::<i32>().unwrap())
+        .collect()
+}
+
+pub fn solution(list: Vec<i32>, remove_node: usize) -> i32 {
+    let root_idx = list.iter().position(|x| *x == -1).unwrap();
+    if root_idx == remove_node {
+        return 0;
     }
+
+    let mut tree_map: BTreeMap<i32, Vec<usize>> = BTreeMap::new();
+
+    for (idx, value) in list.iter().enumerate() {
+        if *value == -1 {
+            continue;
+        }
+
+        if !tree_map.contains_key(value) {
+            tree_map.insert(*value, Vec::new());
+        }
+
+        tree_map.get_mut(value).unwrap().push(idx);
+    }
+
+    tree_map
+        .get_mut(&list[remove_node])
+        .unwrap()
+        .retain(|x| *x != remove_node);
+
+    dfs(root_idx as i32, &tree_map)
+}
+
+pub fn dfs(value: i32, tree_map: &BTreeMap<i32, Vec<usize>>) -> i32 {
+    let mut result = 0;
+    if !tree_map.contains_key(&value) {
+        return 1;
+    }
+    for idx in tree_map.get(&value).unwrap().iter() {
+        result += dfs(*idx as i32, tree_map);
+    }
+
+    return cmp::max(result, 1);
 }
--- p1068/src/main.rs
+++ p1068/src/main.rs
@@ -1,60 +1,9 @@
-use std::{collections::VecDeque, io};
+use p1068::{get_number_io, get_vec_i32_io, solution};
 
 fn main() {
-    let mut buf_str = String::new();
-    io::stdin().read_line(&mut buf_str).unwrap();
+    let _len = get_number_io();
+    let list = get_vec_i32_io();
+    let remove_node = get_number_io() as usize;
 
-    let len: usize = buf_str.trim().parse().unwrap();
-
-    buf_str.clear();
-    io::stdin().read_line(&mut buf_str).unwrap();
-    let mut list: Vec<i32> = buf_str
-        .split_whitespace()
-        .map(|v| v.parse().unwrap())
-        .collect();
-
-    buf_str.clear();
-    io::stdin().read_line(&mut buf_str).unwrap();
-
-    let remove_node: usize = buf_str.trim().parse().unwrap();
-
-    if remove_node == 0 {
-        println!("{}", 0);
-        return;
-    }
-
-    list[remove_node] = -100;
-
-    let mut result = 0;
-
-    let mut queue = VecDeque::new();
-    queue.push_back(0);
-
-    while !queue.is_empty() {
-        let node_num = queue.pop_front().unwrap() as i32;
-        if list[node_num as usize] == -100 {
-            continue;
-        }
-
-        let left = (node_num * 2 + 1) as usize;
-        let right = (node_num * 2 + 2) as usize;
-
-        let mut is_leaf = true;
-        if left < len {
-            if node_num == list[left] {
-                is_leaf = false;
-                queue.push_back(left as i32);
-            }
-        }
-        if right < len {
-            if node_num == list[right] {
-                is_leaf = false;
-                queue.push_back(right as i32);
-            }
-        }
-        if is_leaf {
-            result += 1;
-        }
-    }
-    println!("{}", result);
+    println!("{}", solution(list, remove_node));
 }
